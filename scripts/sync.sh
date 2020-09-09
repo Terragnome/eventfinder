36046f7d7753f1fb0e48b06799934860b16f95d1
Michael Lin <mhuailin@gmail.com>
2020-09-08 21:44:12 -0700

    Remove EventBrite API since it's now deprecated

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,2 @@
 alembic upgrade head
 python -m models.data.connector_tmdb
-python -m models.data.connector_eb --address="San Francisco, CA"
-python -m models.data.connector_eb --address="San Jose, CA"
