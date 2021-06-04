d004d15dc6872d02584eb54705140b264fa8a96f
Michael Lin <mhuailin@gmail.com>
2021-06-04 09:53:25 -0700

    Index tags by both name and type

---
+++ migrations/versions/eb2fe2c4897d_index_tags_by_name_and_type.py
@@ -0,0 +1,25 @@
+"""Index Tags by name and type
+
+Revision ID: eb2fe2c4897d
+Revises: a6d01d6eef29
+Create Date: 2021-06-04 16:48:09.665707
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eb2fe2c4897d'
+down_revision = 'a6d01d6eef29'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_index('tags_by_name')
+    op.create_index('tags_by_name', 'tags', ['tag_name', 'tag_type'])
+
+def downgrade():
+    op.drop_index('tags_by_name')
+    op.create_index('tags_by_name', 'tags', ['tag_name'])
